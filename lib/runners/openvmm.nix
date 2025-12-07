e4a33cf639e835b59605675409f3d8cf99b92c91
Astro <astro@spaceboyz.net>
2025-12-07 02:32:23 +0100

    openvmm: rewrite

--- lib/runners/openvmm.nix
+++ lib/runners/openvmm.nix
@@ -1,51 +1,77 @@
 { pkgs
 , microvmConfig
-, macvtapFds
+, ...
 }:
 
 let
   inherit (pkgs) lib;
   inherit (microvmConfig)
-    user
-    vcpu mem interfaces volumes shares devices vsock
-    kernel initrdPath
+    hostName preStart user
+    vcpu mem balloon initialBalloonMem hotplugMem hotpluggedMem interfaces volumes shares devices vsock
+    kernel initrdPath credentialFiles
     storeDisk storeOnDisk;
+
+  muMsvm = pkgs.stdenv.mkDerivation {
+    pname = "mu-msvm";
+    version = "25.1.4";
+    src = pkgs.fetchurl {
+      url = "https://github.com/microsoft/mu_msvm/releases/download/v25.1.4/RELEASE-X64-artifacts.zip";
+      hash = "0dm6cv84lhwzxva7qsdphdi1fm853lb37b0x658bdrcy82xx2gik";
+    };
+    
+  };
+
 in {
+  preStart = ''
+    ${preStart}
+    export HOME=$PWD
+  '';
+
   command =
     if user != null
     then throw "openvmm will not change user"
+    else if initialBalloonMem != 0
+    then throw "openvmm does not support initialBalloonMem"
+    else if hotplugMem != 0
+    then throw "openvmm does not support hotplugMem"
+    else if hotpluggedMem != 0
+    then throw "openvmm does not support hotpluggedMem"
+    else if credentialFiles != {}
+    then throw "openvmm does not support credentialFiles"
     else builtins.concatStringsSep " " (
       [
         "${pkgs.openvmm}/bin/openvmm"
-        "-m" "${toString mem}M"
+        "--hv"
+        "-m" "${toString mem}MB"
         "-p" (toString vcpu)
+        "--virtio-console"
         "-k" (lib.escapeShellArg "${kernel.dev}/vmlinux")
         "-r" initrdPath
-        "-c" (lib.escapeShellArg "console=ttyS0 reboot=k panic=1 verbose ${toString microvmConfig.kernelParams}")
-        # "--vmbus-redirect"
-        "--hv"
-        # "--virtio-console"
-        "--virtio-serial" "stderr"
-        "--guest-watchdog"
+        "-c" (lib.escapeShellArg "console=hvc0 verbose reboot=k panic=1 ${toString microvmConfig.kernelParams}")
       ]
       ++
       lib.optionals storeOnDisk [
         "--disk" (lib.escapeShellArg "file:${storeDisk},ro")
       ]
       ++
-      builtins.concatMap ({ image, ... }:
-        [ "--disk" (lib.escapeShellArg "file:${image},uh") ]
+      builtins.concatMap ({ serial, image, readOnly, ... }:
+        lib.warnIf (serial != null) ''
+          Volume serial is not supported for openvmm
+        ''
+        [ "--disk"
+          (lib.escapeShellArg "${image}${
+            lib.optionalString readOnly ",ro"
+          }")
+        ]
       ) volumes
       ++
-      builtins.concatMap ({ proto, source, tag, ... }:
-        {
-          virtiofs = [
-            "--virtio-fs" (lib.escapeShellArg "${tag}:${source}")
-          ];
-          "9p" = [
-            "--virtio-9p" (lib.escapeShellArg "${tag}:${source}")
-          ];
-        }.${proto}
+      builtins.concatMap ({ proto, source, tag, readOnly, ... }:
+        if proto == "9p"
+        then if readOnly then
+          throw "openvmm does not support readonly 9p share"
+        else [
+          "--virtio-9p" (lib.escapeShellArg "${source},${tag}")
+        ] else throw "virtiofs shares not implemented for openvmm"
       ) shares
       ++
       builtins.concatMap ({ type, id, mac, ... }:
@@ -53,18 +79,17 @@ in {
         then [
           "--virtio-net" "tap"
         ]
-          # TODO: --nic
         else throw "interface type ${type} is not supported by openvmm"
       ) interfaces
-      ++
-      map ({ ... }:
-        throw "PCI/USB passthrough is not supported on openvmm"
-      ) devices
-      ++ (
-        if vsock.cid != null
-        then throw "Host-native AF_VSOCK is not supported by openvmm"
-        else []
-      )
+      # ++
+      # map ({ bus, path }: {
+      #   pci = lib.escapeShellArg "--vfio-pci=${path}";
+      #   usb = throw "USB passthrough is not supported on openvmm";
+      # }.${bus}) devices
+      # ++
+      # lib.optionals (vsock.cid != null) [
+      #   "--vsock" (toString vsock.cid)
+      # ]
     );
 
   # TODO:
