00113b13b2f1cc846578aa8796c3465991041234
astro <astro@users.noreply.github.com>
2026-09-10 23:15:55 +0000

    deploy: 1b99da49e9d1c8f15fd4911f7b8d2c4375758dcd

--- searcher-09f2665d.js
+++ searcher-09f2665d.js
@@ -440,7 +440,7 @@ window.search = window.search || {};
         if (yes) {
             loadSearchScript(
                 window.path_to_searchindex_js ||
-                path_to_root + 'searchindex-fde9734b.js',
+                path_to_root + 'searchindex-58b03e69.js',
                 'mdbook-search-index');
             search_wrap.classList.remove('hidden');
             searchicon.setAttribute('aria-expanded', 'true');
